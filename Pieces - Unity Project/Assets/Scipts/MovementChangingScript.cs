b92e2c496e685afec527da9ab2e8894a7b2586d4
Maksim Martynov <martynov.maxim@gmail.com>
2021-01-06 21:34:09 +0100

    Basic physics
    Basic physics/movement component added

---
+++ Pieces - Unity Project/Assets/Scipts/MovementChangingScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementChangingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Transform transform;
+    Vector3 velocity;
+    public Vector3 initVel;
+    public float deceleration;
+    void Start()
+    {
+        transform = gameObject.GetComponent<Transform>();
+        velocity = initVel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementCycle();
+        DecelerationCycle();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        velocity = Vector3.Reflect(velocity, collision.GetContact(0).normal);
+    }
+    private void MovementCycle()
+    {
+        transform.position += velocity * Time.deltaTime;
+    }
+
+    void DecelerationCycle()
+    {
+        if (velocity.magnitude != 0)
+        {
+            float speed = velocity.magnitude;
+            velocity *= (speed - deceleration * Time.deltaTime) / speed;
+        }
+    }
+
+    public void SetSpeed(Vector3 newVelocity)
+    {
+        velocity += newVelocity;
+    }
+}
