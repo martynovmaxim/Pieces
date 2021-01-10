3ec585d457251f7cf1ca4f66fa9657cb882e6a5f
Maksim Martynov <martynov.maxim@gmail.com>
2021-01-10 14:56:25 +0100

    Input

---
+++ Pieces - Unity Project/Assets/Scipts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public MovementScript movement;
+
+    public PlayerInput controls;
+
+    bool pressedSpace = false;
+    float speed;
+    public float speedIncrease;
+    public float minSpeed = 0f;
+    public float maxSpeed = 15f;
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+    private void Awake()
+    {
+        controls = new PlayerInput();
+        controls.GameInput.Rotate.performed += ctx => Rotate(ctx.ReadValue<float>());
+        controls.GameInput.Charge.performed += _ => Charge();
+        controls.GameInput.Launch.performed += _ => Launch();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //read values
+        if (pressedSpace)
+        {
+            speed = Mathf.Clamp(speed + speedIncrease * Time.deltaTime, minSpeed, maxSpeed);
+            Debug.Log(speed);
+        }
+        //
+
+    }
+
+    void Launch()
+    {
+        Debug.Log("LAUNCH!");
+        pressedSpace = false;
+    }
+
+    void Charge()
+    {
+        Debug.Log("Start charging");
+        pressedSpace = true;
+    }
+    void Rotate(float value)
+    {
+        
+        Debug.Log("Rot " + value);
+    }
+}
