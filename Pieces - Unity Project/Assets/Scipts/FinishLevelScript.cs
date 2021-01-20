a5a048b8c6b222e713899e0778b823efed2f8103
Maksim Martynov <martynov.maxim@gmail.com>
2021-01-20 22:11:57 +0100

    Finish level

---
+++ Pieces - Unity Project/Assets/Scipts/FinishLevelScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLevelScript : MonoBehaviour
+{
+    public AudioClip FinishMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audio = GameObject.FindGameObjectWithTag("BackgroundMusic").GetComponent<AudioSource>();
+        audio.Stop();
+        audio.clip = FinishMusic;
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
